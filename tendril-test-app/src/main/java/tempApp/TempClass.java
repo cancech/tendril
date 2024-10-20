174bf31459a13efc8905e8a23534aec941420bb8
Jaroslav Bosak <cancech@nowhere.com>
2024-10-20 14:23:08 -0400

    Adding test app for testing the libraries

---
+++ tendril-test-app/src/main/java/tempApp/TempClass.java
@@ -0,0 +1,23 @@
+package tempApp;
+
+import tendril.bean.Provider;
+
+@Provider
+@MyTypeId(MyType.VAL1)
+public class TempClass {
+
+	private final String val;
+	
+	public TempClass(String val) {
+		this.val = val;
+	}
+	
+	public String getValue() {
+		return val;
+	}
+	
+	@Override
+	public String toString() {
+		return "TempClass Value = " + val;
+	}
+}
