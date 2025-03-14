0194f216d90fa41624e4383d6fa1c424bfd036d8
Jaroslav Bosak <cancech@gmail.com>
2025-03-14 18:27:50 -0400

    Adding missed class

---
+++ tendril-di/src/main/java/tendril/bean/Configuration.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2025 Jaroslav Bosak
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/license/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tendril.bean;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation which denotes a class which is a configuration for the purpose of producing beans. It is not a bean in its own right, but rather allows for the creation of
+ * beans from appropriately annotated methods within.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Configuration {
+
+}
