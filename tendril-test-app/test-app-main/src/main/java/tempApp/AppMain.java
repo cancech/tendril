fbecb9249ca9ae826588dfb5f69f45c7bcfe4a0f
Jaroslav Bosak <cancech@gmail.com>
2025-04-01 19:05:21 -0400

    Refactoring build.gradle to avoid duplication

--- tendril-test-app/test-app-main/src/main/java/tempApp/AppMain.java
+++ tendril-test-app/test-app-main/src/main/java/tempApp/AppMain.java
@@ -5,6 +5,7 @@ import tendril.context.ApplicationContext;
 public class AppMain {
 	
 	public static void main(String[] args) {
+	    // TODO convert into something which can be run as a "test"
         ApplicationContext ctx = new ApplicationContext();
         ctx.start();
 	}
